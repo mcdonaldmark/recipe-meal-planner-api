0391cf1b4a756f00ed1e6924cb7bec12aa1e56c9
mcdonaldmark <markamcdonald7@gmail.com>
2026-02-05 03:11:36 -0700

    sync

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const recipesController = require('../controllers/recipesController'); // updated
 const ensureAuthenticated = require('../middleware/auth');
 
-router.get('/', recipesController.getAllRecipes);
-router.get('/:id', recipesController.getRecipeById);
+router.get('/', ensureAuthenticated, recipesController.getAllRecipes);
+router.get('/:id', ensureAuthenticated, recipesController.getRecipeById);
 router.post('/', ensureAuthenticated, recipesController.createRecipe);
 router.put('/:id', ensureAuthenticated, recipesController.updateRecipe);
 router.delete('/:id', ensureAuthenticated, recipesController.deleteRecipe);
