e88e759c7fc1d896684f8b311bd785513727626c
mcdonaldmark <markamcdonald7@gmail.com>
2026-02-04 22:00:54 -0700

    sync

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 
 router.get('/', usersController.getAllUsers);
 router.get('/:id', usersController.getUserById);
-router.post('/', usersController.createUser);   // <-- ADD THIS
+router.post('/', usersController.createUser);
 router.put('/:id', usersController.updateUser);
 router.delete('/:id', usersController.deleteUser);
 
